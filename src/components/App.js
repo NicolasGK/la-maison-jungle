0f7e603e8877f7326e7adaafaf88be13630b9671
NicolasGK <104904015+NicolasGK@users.noreply.github.com>
2023-07-13 12:20:39 +0200

    analytics

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import Cart from './Cart'
 import Footer from './Footer'
 import ShoppingList from './ShoppingList'
 import '../styles/Layout.css'
+import { Analytics } from '@vercel/analytics/react'
 
 
 function App() {
@@ -18,13 +19,14 @@ function App() {
 		<div> 
 			<Banner>
 				<img src={logo} alt='logo-la-maison-jungle' className='lmj-logo' />
-				<h1 className='lmj-title'>- La maison jungle -</h1>
+				<h1 className='lmj-title'>La maison jungle</h1>
 			</Banner>
 			<div className='lmj-layout-inner'>
 				<Cart cart={cart} updateCart={updateCart} />
 				<ShoppingList cart={cart} updateCart={updateCart} />
 			</div>
 			<Footer />
+			<Analytics />
 		</div>
 	)
 }
